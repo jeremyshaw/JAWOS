f4f733604533ad1a8c2ba5f2110af81388da17a4
Jeremy Shaw <jeremyshaw1@gmail.com>
2019-09-18 18:11:02 -0700

    some preliminary work on proc.c. Main loop and Init is still incomplete

--- p2/p-code/proc.c
+++ p2/p-code/proc.c
@@ -6,27 +6,39 @@
 #include "syscall.h"     // sys service calls
 #include "tools.h"       // Number2Str()
 
+#define CHR_ARY 20
+
 void Idle(void) {   // Idle thread, flashing a dot on the upper-left corner
-   ...
+...
 }
 
 void Init(void) {  // Init, PID 1, asks/tests various OS services
-   declare two integers: my_pid & os_time
-   declare two 20-char arrays: pid_str & time_str
+	// declare two integers: my_pid & os_time
+	int my_pid;
+	int os_time;
 
-   call sys_get_pid() to get my_pid
-   call Number2Str() to convert it to pid_str
+	// declare two 20-char arrays: pid_str & time_str
+	char pid_str[CHR_ARY];
+	char time_str[CHR_ARY];
 
-   forever loop {
-      call sys_write() to show "my PID is "
-      call sys_write() to show my pid_str
-      call sys_write to show "... "
-      call sys_sleep() to sleep for 1 second
-      call sys_get_time() to get current os_time
-      call Number2Str() to convert it to time_str
-      call sys_write() to show "sys time is "
-      call sys_write() to show time_str
-      call sys_write to show "... "
-      call sys_sleep() to sleep for 1 second
-   }
+
+	// ?? check back on this ?? call sys_get_pid() to get my_pid
+	my_pid = sys_get_pid();
+	
+	// ?? check back on this ?? call Number2Str() to convert it to pid_str
+	Number2Str(my_pid, &pid_str);
+
+	forever loop 
+	while(1){
+		call sys_write() to show "my PID is "
+		call sys_write() to show my pid_str
+		call sys_write to show "... "
+		call sys_sleep() to sleep for 1 second
+		call sys_get_time() to get current os_time
+		call Number2Str() to convert it to time_str
+		call sys_write() to show "sys time is "
+		call sys_write() to show time_str
+		call sys_write to show "... "
+		call sys_sleep() to sleep for 1 second
+	}
 }
