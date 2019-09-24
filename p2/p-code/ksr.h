3153661c351d206839d290bae2f31248078fbff2
Jeremy Shaw <jeremyshaw1@gmail.com>
2019-09-23 18:03:05 -0700

    adding a lot of header files

---
+++ p2/p-code/ksr.h
@@ -0,0 +1,15 @@
+// ksr.h, 159
+
+// Team Name: JAWOS (Members: Alex Leones, Jeremy Shaw, William Guo)
+
+#ifndef _KSR_
+#define _KSR_
+
+#include "const-type.h"//for func_p_t
+
+void SpawnSR(func_p_t p);
+void TimerSR(void);
+void SyscallSR(void);
+void SysSleep(void);
+
+#endif
